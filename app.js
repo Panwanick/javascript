227ab979d2858a7a72af8f793c130776cb09c9ae
Panwanick <panwanixk@gmail.com>
2025-08-11 13:32:02 +0700

    add app.js and index.html

---
+++ app.js
@@ -0,0 +1,93 @@
+// document.write("Hello World")
+// document.write("<h1>Good Morning World</h1>")
+
+// // alert("Hello Alert")
+// console.log("Hello World")
+// console.log("1234")
+// console.log(1234)
+
+// console.error("Virus Error")
+// console.warn("Test Warning Hello")
+
+// let name = 200
+// console.log(name)
+// let _1name = 300
+// console.log(_1name)
+// _2name = 400
+// console.log(_2name)
+// let $if = 500
+// console.log($if)
+
+// const num = 123
+// console.log(num)
+// num = 456
+
+
+//comment
+//console.log("Hello World")
+//console.log("Hello World")
+//console.log("Hello World")
+//console.log("Hello World")
+//console.log("Hello World")
+//console.log("Hello World")
+//console.log("Hello World")
+
+// let num = 123
+// console.log(typeof(num))
+// let score = 20.12
+// console.log(typeof(score))
+// let test
+// console.log(test)
+// // let hello = "Hello"
+// // console.log(hello)
+
+// let a = 'Hello'
+// let b = "javascript"
+// console.log(a,b)
+
+// let x = '1.2'
+// let y = parseInt(x)
+// console.log(y)
+// console.log(typeof(y))
+// console.log("------------")
+// let z = parseFloat(x)
+// console.log(z)
+// console.log(typeof(z))
+
+// let point = 200
+// console.log(typeof(point))
+// point = point.toString()
+// console.log(typeof(point))
+
+// let point1 = 200 + ""
+// console.log(typeof(point))
+
+
+let myarray = new Array()
+console.log(myarray)
+console.log(typeof(myarray))
+myarray[0] = 200
+myarray[1] = 300
+console.log(myarray)
+
+let mynumber = Array(100,200,300,400,500)
+console.log(mynumber)
+
+let num1 = 100+100
+let num2 = 100-100
+let num3 = 100*100
+let num4 = 100/100
+let num5 = 100%100
+console.log(num1)
+console.log(num2)
+console.log(num3)
+console.log(num4)
+console.log(num5)
+
+let a = 10
+let b = 20
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
