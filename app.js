558519d0fa7a4fded3be6fe91c36115d59fbd13c
Panwanick <panwanixk@gmail.com>
2025-08-11 15:02:24 +0700

    edit app.js

--- app.js
+++ app.js
@@ -63,31 +63,118 @@
 // console.log(typeof(point))
 
 
-let myarray = new Array()
-console.log(myarray)
-console.log(typeof(myarray))
-myarray[0] = 200
-myarray[1] = 300
-console.log(myarray)
+// let myarray = new Array()
+// console.log(myarray)
+// console.log(typeof(myarray))
+// myarray[0] = 200
+// myarray[1] = 300
+// console.log(myarray)
 
-let mynumber = Array(100,200,300,400,500)
-console.log(mynumber)
+// let mynumber = Array(100,200,300,400,500)
+// console.log(mynumber)
 
-let num1 = 100+100
-let num2 = 100-100
-let num3 = 100*100
-let num4 = 100/100
-let num5 = 100%100
-console.log(num1)
-console.log(num2)
-console.log(num3)
-console.log(num4)
-console.log(num5)
+// let num1 = 100+100
+// let num2 = 100-100
+// let num3 = 100*100
+// let num4 = 100/100
+// let num5 = 100%100
+// console.log(num1)
+// console.log(num2)
+// console.log(num3)
+// console.log(num4)
+// console.log(num5)
 
-let a = 10
-let b = 20
-console.log(a+b)
-console.log(a-b)
-console.log(a*b)
-console.log(a/b)
-console.log(a%b)
+// let a = 10
+// let b = 20
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+// console.log(a**b)
+
+// console.log(a == b)
+// console.log(a === b)
+// console.log(a != b)
+// console.log(a > b)
+// console.log(a < b)
+// console.log(a >= b)
+// console.log(a <= b)
+
+// let a = true
+// let b = false
+// console.log(a && b)
+// console.log(a || b)
+// console.log(!a)
+// console.log(!b)
+
+// let x = 1
+// let y = 1
+// // Prefix
+// console.log("Now: ",x)
+// console.log("Prefix:", ++x)
+// console.log("After: ", x)
+// console.log("------")
+// console.log("Now: ", x)
+// console.log("Prefix: ", --x)
+// console.log("After: ", x)
+// console.log("------")
+// // Postfix
+// console.log("Now: ", y)
+// console.log("Postfix: ", y++)
+// console.log("After: ", y)
+// console.log("------")
+// console.log("Now: ", y)
+// console.log("Postfix: ", y--)
+// console.log("After: ", y)
+
+// let a = 2
+// let b = 4
+// console.log(a+=b)
+// console.log(a-=b)
+// console.log(a*=b)
+// console.log(a/=b)
+// console.log(a%=b)
+
+// Example 1
+// age = 16
+// if(age >= 15 && age <= 25){
+//     console.log("มีอายุอยู่ในช่วงวัยรุ่นจริง")
+// }
+
+// // Example 2
+// let balance = 1000
+// let withdrawal = 200
+// if(withdrawal <= balance){
+//     console.log("เงินที่สามารถถอนได้: ", balance)
+//     console.log("จำนวนเงินที่จะถอน: ", withdrawal)
+//     balance -= withdrawal
+//     console.log("จำนวนเงินคงเหลือ: ", balance)
+// }
+
+// // โจทย์ปัญหา
+// let username = "admin"
+// let password = "12345"
+// if(username == "admin" && password == "1234"){
+//     alert("เข้าสู่ระบบสำเร็จ")
+// }else{
+//     alert("Username และ Password ไม่ถูกต้อง")
+// }
+
+let score = 81
+if(score > 80 && score < 100){
+    console.log("Grade: A")
+}
+else if(score > 70 && score < 80){
+    console.log("Grade: B")
+}
+else if(score > 60 && score < 70){
+    console.log("Grade: C")
+}
+else if(score > 50 && score < 60){
+    console.log("Grade: D")
+}
+else if(score < 50){
+    console.log("Grade: F")
+}
+
